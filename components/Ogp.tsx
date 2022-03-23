73ab17932d5c0fa11c232f2e2b0b34f567e0e72f
notch_man <s2113591@klis.tsukuba.ac.jp>
2022-03-23 16:32:36 +0900

    next.jsでプロジェクトを立て直し

---
+++ components/Ogp.tsx
@@ -0,0 +1,19 @@
+import { VFC } from "react";
+
+interface MetaData {
+  pageTitle?: string;
+  pageDescription?: string;
+  pageUrl?: string;
+  pageImg?: string;
+  pageWidth?: number;
+  pageHeight?: number;
+}
+
+// const Ogp: VFC<MetaData> = ({
+//   pageTitle,
+//   pageDescription,
+//   pagePath,
+//   pageImg,
+//   pageImgWidth,
+//   pageImdHeight,
+// }) => {};
