ef82d3d5deb965d5636b1ab38d91a94b4aed9035
notch_man <miyake.kentaro0815@mokkuriko.com>
2023-05-31 01:07:21 +0900

    feature/remove-tailwind

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
-/** @type {import('next').NextConfig} */
+const withLinaria = require('next-with-linaria');
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+  reactStrictMode: true, experimental: {
+    appDir: true,
+  }, images: {
+    remotePatterns: [{
+      protocol: "https", hostname: "**",
+    },],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withLinaria(nextConfig);
